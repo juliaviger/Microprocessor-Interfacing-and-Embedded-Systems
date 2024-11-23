2556f06e3a2625a37869ae32eb3fb59013c6299e
Julia Viger <144358015+juliaviger@users.noreply.github.com>
2024-11-23 15:52:29 -0500

    Update chario.h

--- chario.h
+++ chario.h
@@ -2,5 +2,9 @@
 #define _CHARIO_H_
 
 /* place character-I/O function prototypes here */
+unsigned int GetChar(void);
+unsigned int PrintChar(int ch);
+unsigned int PrintString(char *str);
+void PrintHexDigit(unsigned int n);
 
 #endif /* _CHARIO_H_ */
